e8487c1c9798bea70001dd443070570c9e06444e
Andrey Ilinykh <andrey.ilinykh@B-29TXMD6M-0034.local>
2021-04-29 13:13:56 +0300

    new branch + gitignore

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,66 +7,47 @@ public class Main {
     public static void main(String[] args) {
 
         HashMap<String, Integer> mappp = new HashMap<>();
-        namesBdaysVoid("Вася", "1989", mappp);
+        //namesBdaysVoid("Вася", "1989", mappp);
         System.out.println(mappp.entrySet());
 
 
-        HashMap<String, Integer> item1 = namesBdays("Петр1", "1996");
-        HashMap<String, Integer> item2 = namesBdays("Петр2", "1993");
-        HashMap<String, Integer> item3 = namesBdays("Петр3", "1990");
-        HashMap<String, Integer> item4 = namesBdays("Петр4", "sdaf");
-        HashMap<String, Integer> item5 = namesBdays("Петр5", "1991");
+        namesBdaysVoid("Петр1", "1996", mappp);
+        namesBdaysVoid("Петр2", "1993", mappp);
+        namesBdaysVoid("Петр3", "1990", mappp);
+        namesBdaysVoid("Петр4", "sdaf", mappp);
+        namesBdaysVoid("Петр5", "1991", mappp);
 
-        Map<String, Integer> hashMap = new HashMap<>();
-        item1.forEach(hashMap::put);
-        item2.forEach(hashMap::put);
-        item3.forEach(hashMap::put);
-        item5.forEach(hashMap::put);
 
-        System.out.println(hashMap);
+        System.out.println(mappp);
         System.out.println();
-        hashMap.entrySet().forEach(System.out::println);
+        mappp.entrySet().forEach(System.out::println);  //set
 
-        hashMap.forEach((k, v) -> System.out.println("Имя:" + k + " " + "Год:" + v));
+        mappp.forEach((k, v) -> System.out.println("Имя:" + k + " " + "Год:" + v));
 
         String min;
-        min = hashMap
+        min = mappp
                 .entrySet()
                 .stream()
-                .filter(v -> v.getValue() != null)
+                .filter(entry -> entry.getValue() != null)
                 .min(Map.Entry.comparingByValue())
-                .get()
+                .get() //Optional
                 .getKey();
 
 
-        System.out.println("Самый старший:" + (min) + " " + "Год рождения:" + hashMap.get(min));
+        System.out.println("Самый старший:" + (min) + " " + "Год рождения:" + mappp.get(min));
 
-
-    }
-
-    public static HashMap<String, Integer> namesBdays(String name, String bday) {
-
-        Integer intBday;
-
-        try {
-            intBday = Integer.parseInt(bday);
-        } catch (NumberFormatException e) {
-            intBday = null;
-        }
-        HashMap<String, Integer> namesBdays = new HashMap<String, Integer>();
-        namesBdays.put(name, intBday);
-        return namesBdays;
 
     }
 
     public static void namesBdaysVoid(String name, String bday, HashMap<String, Integer> mapp) {
 
-        Integer intBday;
+
+        Integer intBday = null;
 
         try {
-            intBday = Integer.parseInt(bday);
+            intBday = Integer.parseInt(bday); //checked unchecked exception java
         } catch (NumberFormatException e) {
-            intBday = null;
+            System.out.println("NumberFormatException " + e); //нужен лог
         }
 
         mapp.put(name, intBday);
@@ -75,3 +56,11 @@ public class Main {
 
 
 }
+
+/* новая ветка
+коммит
++ месаге
+пуш
+.iml .idea все это в коммите добавить в игнор + удалить в репе все это
+коммит
+ */
